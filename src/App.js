2011db2f183a2f47a2409f6a57a8a56250f575c1
matthewberzinskas <pasomatt@gmail.com>
2022-04-26 13:09:36 -0700

    Bootstrap styling

--- src/App.js
+++ src/App.js
@@ -90,28 +90,34 @@ function ChatRoom() {
 function ChatMessage(props) {
   const { displayName, text, uid, photoURL, createdAt } = props.message;
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
-  let created = new Date(createdAt.seconds * 1000).toLocaleDateString("en-us", {
-    weekday: "long",
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
+  console.log(JSON.stringify(props.message, null, 3));
+
+  let created;
+  if (createdAt !== null) {
+    console.log(createdAt.seconds);
+    created = new Date(createdAt.seconds * 1000).toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+  }
 
   return (
     <>
-      <div class="row border">
-        <div class="col-md-2 col-3">
+      <div
+        className={`message d-flex ${
+          messageClass === "sent" ? "flex-row" : "flex-row-reverse"
+        } border p-2`}
+      >
+        <div className="text-center col-md-2 col-3 p-1">
           <img src={photoURL} referrerPolicy="no-referrer" />
         </div>
-        <div class="col-md-10 col-9">
-          <div class="pb-3">
-            <div className={`message ${messageClass}`}>
-              <p>{text}</p>
-            </div>
-          </div>
-          <div class="row border">
-            <div class="col-md-6 border">Posted By: {displayName}</div>
-            <div class="col-md-6 border">At: {created.toString()}</div>
+        <div className="col-md-10 col-9 p-1">
+          <p>{text}</p>
+          <div className="row border p-1">
+            <div className="col-md-6 border">Posted By: {displayName}</div>
+            <div className="col-md-6 border">At: {created}</div>
           </div>
         </div>
       </div>
