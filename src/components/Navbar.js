4d977f60cab2dd064b247d7a8befe13327b305c9
matthewberzinskas <pasomatt@gmail.com>
2022-04-26 13:16:42 -0700

    Code cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           React-Chat
         </a>
         <div className="justify-content-end">
