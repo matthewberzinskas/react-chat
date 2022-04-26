a66586ec8484d7bf0edc7d158e7678dadaa277fa
matthewberzinskas <pasomatt@gmail.com>
2022-04-26 15:38:34 -0700

    Updates to styling

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -17,22 +17,38 @@ export default function ChatMessage(props) {
 
   return (
     <>
-      <div
-        className={`message d-flex ${
-          messageClass === "sent" ? "flex-row" : "flex-row-reverse"
-        } border p-2`}
-      >
-        <div className="text-center col-md-2 col-4 p-1">
+      <div class="container-fluid border p-2">
+        <div class="row">
+          <div class="col-3 col-sm-2 col-xl-1">
+            <img
+              className="profile-pic"
+              src={photoURL}
+              referrerPolicy="no-referrer"
+              alt={`of ${displayName}`}
+            />
+          </div>
+          <div class="col-9 col-sm-10 col-xl-11">
+            <div class="row">
+              <div class="col-12 text-muted">{displayName}</div>
+            </div>
+            <div class="row">
+              <div class="col-12 lead">{text}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/*         <div className="text-center col-md-2 col-4">
           <img className="profile-pic" src={photoURL} referrerPolicy="no-referrer" alt={`of ${displayName}`} />
         </div>
-        <div className="col-md-10 col-8 p-1">
+        <div className="col-md-10 col-8">
           <p>{text}</p>
-          <div className="row border p-1">
+          <div className="row border">
             <div className="col-md-6 border">Posted By: {displayName}</div>
             <div className="col-md-6 border">At: {created}</div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
